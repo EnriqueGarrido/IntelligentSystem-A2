22132f198b75590406ba9993607d8cca1bd73c0f
Enrique Garrido Pozo <EnriqueGarrido@users.noreply.github.com>
2017-11-01 18:22:53 +0100

    comment
    comentarios

--- InteligentesLab3/src/Frontier.java
+++ InteligentesLab3/src/Frontier.java
@@ -7,28 +7,48 @@ public class Frontier {
 	///////////////////////////////////////////////////// LINKED LIST ///////////////////////////////////////////////////
 	private LinkedList<Node> frontier;
 	
-	
+	/**************************************************************************************************
+	 * Method: getFrontier
+	 * Description: It gets the frontier
+	 * @return frontier -> the frontier in each moment of the tree
+	 **************************************************************************************************/
 	public LinkedList<Node> getFrontier() {
 		return frontier;
 	}
-
+    /***************************************************************************************************
+     * Method: setFrontier
+     * Description: You can set a new frontier with this method
+     * @param frontier -> the frontier that you are going to change the value
+     ***************************************************************************************************/
 	public void setFrontier(LinkedList<Node> frontier) {
 		this.frontier = frontier;
 	}
-
+    /*****************************************************************************************************
+     * Method: createFrontier
+     * Description: This method is in charge of build a list with all frontiers
+     *****************************************************************************************************/
 	public void createFrontier() {
 		frontier = new LinkedList<Node>();
-		
 	}
-	
+	/*****************************************************************************************************
+	 * Method: insertNode
+	 * Description: You can insert a new node to the frontier
+	 * @param node -> Is the node that you are going to insert to the list
+	 *****************************************************************************************************/
 	public void insertNode(Node node) {
 		frontier.add(node);
 	}
-	
+	/*******************************************************************************************************
+	 * Method: removeFirst
+	 * Description: You can remove a node in the frontier
+	 ******************************************************************************************************/
 	public void removeFirst() {
 		frontier.removeFirst();
 	}
-	
+	/*******************************************************************************************************
+	 * Method: isEmpty
+	 * Description: You can prove if the frontier is empty
+	 *******************************************************************************************************/
 	public void isEmpty() {
 		frontier.isEmpty();
 	}
@@ -36,24 +56,41 @@ public class Frontier {
 	/////////////////////////////////////////////////////// QUEUES //////////////////////////////////////////////////
 	
 	private PriorityQueue<Node> queue;
-	
+	/**************************************************************************************************
+	 * Method: getFrontier
+	 * Description: It gets the frontier
+	 * @return frontier -> the frontier in each moment of the tree
+	 **************************************************************************************************/
 	public PriorityQueue<Node> getFrontierQ() {
 		return queue;
 	}
-
+	/*****************************************************************************************************
+     * Method: createFrontier
+     * Description: This method is in charge of build a list with all frontiers
+     *****************************************************************************************************/
 	public void createFrontierQ() {
 		queue = new PriorityQueue<Node>();
 		
 	}
-	
+	/*****************************************************************************************************
+	 * Method: insertNode
+	 * Description: You can insert a new node to the frontier
+	 * @param node -> Is the node that you are going to insert to the list
+	 *****************************************************************************************************/
 	public void insertNodeQ(Node node) {
 		queue.add(node);
 	}
-	
+	/******************************************************************************************************
+	 * Method: removeFirstQ
+	 * Description: You can remove the node in the frontier
+	 ******************************************************************************************************/
 	public void removeFirstQ() {
 		queue.peek();
 	}
-	
+	/******************************************************************************************************
+	 * Method: isEmptyQ
+	 * Description: Is a method to prove that the list is empty or not
+	 *******************************************************************************************************/
 	public void isEmptyQ() {
 		queue.isEmpty();
 	}
--- InteligentesLab3/src/Node.java
+++ InteligentesLab3/src/Node.java
@@ -11,7 +11,12 @@ public class Node implements Comparable<Node>{
 	private int depth;
 	private int value;
 	
-	
+	/**********************************************************************************************************
+	 * Method name: Node
+	 * Description: Constructor of the class
+	 * @param parentNode -> a node that is the parent of the other node 
+	 * @param nextAction -> the action that to the node will be applicated
+	 **********************************************************************************************************/
 	public Node(Node parentNode, Action nextAction) {
 		this.state =  new Field(parentNode.getState(), nextAction);
 		this.action = nextAction;
@@ -21,62 +26,116 @@ public class Node implements Comparable<Node>{
 		this.depth = parentNode.getDepth()+1;
 		this.value = Math.abs(1+(new Random()).nextInt()%100);
 	}
-
+	/************************************************************************************************************
+	 * Method name: Node
+	 * Description: Another constructor of the class
+	 * @param state -> the actual state of the node
+	 ************************************************************************************************************/
 	public Node(Field state) {
 		this.state = state;
 	}
-
-	
-
+    /**************************************************************************************************************
+     * Method name: getParent
+     * Description: The method to know the parent of a node
+     * @return parent -> The actual parent of the node
+     ***************************************************************************************************************/
 	public Node getParent() {
 		return parent;
 	}
-
+    /***************************************************************************************************************
+     * Method name: setParent
+     * Description: You can change the parent with this method
+     * @param parent -> the parent of the node
+     ****************************************************************************************************************/
 	public void setParent(Node parent) {
 		this.parent = parent;
 	}
-
+    /*****************************************************************************************************************
+     * Method name: getState
+     * Description: To know the state of the node
+     * @return node -> the state of the node
+     *****************************************************************************************************************/
 	public Field getState() {
 		return state;
 	}
-
+    /******************************************************************************************************************
+     * Method name: setState
+     * Description: To modify the state of a node
+     * @param state -> the state of the node
+     ******************************************************************************************************************/
 	public void setState(Field state) {
 		this.state = state;
 	}
-
+    /*******************************************************************************************************************
+     * Method name: getCost
+     * Description: To know the cost of a node
+     * @return cost -> the cost of a node
+     ********************************************************************************************************************/
 	public int getCost() {
 		return cost;
 	}
-
+    /********************************************************************************************************************
+     * Method name: setCost
+     * Description: To modify the cost of a node
+     * @param cost -> the cost of a node
+     ********************************************************************************************************************/
 	public void setCost(int cost) {
 		this.cost = cost;
 	}
-
+    /********************************************************************************************************************
+     * Method name: getAction
+     * Description: To know the cost of the action
+     * @return action -> the action of the node
+     ********************************************************************************************************************/
 	public Action getAction() {
 		return action;
 	}
-
+    /*******************************************************************************************************************
+     * Method name: setAction
+     * Description: To modify the action of a node
+     * @param action -> the action of the node
+     ********************************************************************************************************************/
 	public void setAction(Action action) {
 		this.action = action;
 	}
-
+    /********************************************************************************************************************
+     * Method name: getDepth
+     * Description: To get the depth of the node
+     * @return action -> the action of the node
+     ********************************************************************************************************************/
 	public int getDepth() {
 		return depth;
 	}
-
+    /*********************************************************************************************************************
+     * Method name: setDepth
+     * Description: To modify the depth of the node
+     * @param depth -> the depth of the node
+     ********************************************************************************************************************/
 	public void setDepth(int depth) {
 		this.depth = depth;
 	}
-
+    /**********************************************************************************************************************
+     * Method name: getValue
+     * Description: To get the value of the node
+     * @return value -> the value of the node
+     **********************************************************************************************************************/
 	public int getValue() {
 		return value;
 	}
-
+    /***********************************************************************************************************************
+     * Method name: setValue
+     * Description: To modify the value of the node
+     * @param value -> the value of the node
+     ***********************************************************************************************************************/
 	public void setValue(int value) {
 		this.value = value;
 	}
-	
 	@Override
+	/***************************************************************************************************************************
+	 *Method name: compareTo
+	 *Description: To compare one node to another node
+	 *@param other -> a node to compare witb another 
+	 **************************************************************************************************************************/
 	public int compareTo(Node other) { 
 		if(this.getValue()<other.getValue()){
 			return -1;
@@ -86,7 +145,10 @@ public class Node implements Comparable<Node>{
 		}
 		return 0;
 	}
-	
+	/****************************************************************************************************************************
+	 * Method name: toString
+	 * Description: To transform all attributes in a string to print that
+	 ****************************************************************************************************************************/
 	public String toString() {
 		/*return "Node:\n" + state.saveMatrix() + "("+ state.getXt() + ", " + state.getYt() + 
 				")\nCost: "+cost + "\nAction: " + action.toString() + "\nDepth: " + depth + 
